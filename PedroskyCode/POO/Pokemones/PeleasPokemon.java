acc498d33e9313ffb18f12ef65034c781557bd1e
Pedro Segovia <157068737+Seggov@users.noreply.github.com>
2026-04-07 12:32:00 -0400

    Add files via upload

---
+++ PedroskyCode/POO/Pokemones/PeleasPokemon.java
@@ -0,0 +1,61 @@
+public class PeleasPokemon{
+
+    public static void main(String arg[]){
+        estructura();
+
+
+
+
+
+
+
+
+
+    }
+
+
+    public static void estructura(){
+
+
+
+
+    }
+
+
+
+    public static int seleccion(){
+
+
+        
+        return int 2;
+
+
+
+    }
+
+
+    public static void menu(){
+        String menudespleagado = "1 ";
+        System.out.println(menudespleagado);
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
---
+++ PedroskyCode/POO/pedrosky/ITI.java
@@ -0,0 +1,42 @@
+public class ITI {
+
+    public static void main(String[] args){
+        clasificador();
+
+
+
+    }
+
+
+
+
+    public static void clasificador(){
+        menuSemestres();
+
+    }
+
+    public static void menuSemestres(){
+        String semestresOrdenanos = " I) Primer Semestre \n II) Segundo Semestre \n III) Tercer Semestre \n IV) Cuarto Semestre \n V) Quinto Semestre \n VI) Sexto Semestre \n VII) Septimo semestre \n VIII) Octavo Semestre";
+        System.out.println(semestresOrdenanos);
+        
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
