49d3bf087fc0e5661994af6fec46dad7c216dd69
Pedro Segovia <157068737+Seggov@users.noreply.github.com>
2026-04-07 17:56:03 -0400

    Update one.java

--- PedroskyCode/POO/EjerciciosPequeños/1#/one.java
+++ PedroskyCode/POO/EjerciciosPequeños/1#/one.java
@@ -1,1 +1,31 @@
-public class one{
+public class pokemon{
+    
+    
+    // Aqui se guardara la informacion 
+    private String nombre;
+    private String tipo;
+    private int vida;
+    private int ataque;
+    private int defensa;
+
+
+
+
+    public pokemon(String nombre, String tipo, int vida, int ataque, int defensa){
+
+
+        // this es el dedo que apunta a hacia una variable
+        this.nombre = nombre; 
+        this.tipo = tipo;
+        this.vida = vida;
+        this.ataque = ataque;
+        this.defensa = defensa;
+
+
+    }
+
+
+    
+
+
+}
